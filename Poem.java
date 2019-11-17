4f97ce46ddc6eb6c83509e7c78ec00a2c128c159
JST165411 <57699352+JST165411@users.noreply.github.com>
2019-11-17 21:32:34 +0800

    Update Poem.java

--- Poem.java
+++ Poem.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Poem {
 public static void main(String[] args)
 {
- System.out.print("ÇëÊäÈë¡¶³¤ºŞ¸è¡·\n");
+ System.out.print("è¯·è¾“å…¥ã€Šé•¿æ¨æ­Œã€‹\n");
  Scanner p=new Scanner(System.in);
  String temp=p.nextLine();
  StringBuffer str=new StringBuffer(temp);
@@ -13,7 +13,7 @@ public static void main(String[] args)
  str0=Add(str);
  String str1=new String(str0);
  System.out.print(str1);
- System.out.print("ÇëÊäÈëÄúÒª²éÕÒµÄ×Ö\n");
+ System.out.print("è¯·è¾“å…¥æ‚¨è¦æŸ¥æ‰¾çš„å­—\n");
  Scanner p1=new Scanner(System.in);
  String temp1=p1.nextLine();
  int i=Find(temp,temp1);
@@ -24,10 +24,10 @@ public static StringBuffer Add(StringBuffer str) {
   for(int i=0;i<str.length()/7;i++){
     str1.append(str.substring(7*i,7*(i+1)));
     if((i+1)%2!=0){
-     str1.append("£¬");
+     str1.append("ï¼Œ");
     }
     else{
-     str1.append("¡£").append("\n");
+     str1.append("ã€‚").append("\n");
     }
   }
   return str1;
@@ -37,4 +37,8 @@ public static int Find(String orgstr,String findstr){
  while(orgstr.indexOf(findstr)!=-1){
   if(orgstr.indexOf(findstr)!=-1){
    x++;
-   orgstr=orgstr.substring(orgstr.indexOf(findstr)+findstr.l
+   orgstr=orgstr.substring(orgstr.indexOf(findstr)+findstr.length());}
+  return x;
+   }
+}
+}
